b160fd94fc64486a293b8554613a047b3dbb9513
Bruno Coutinho <bscoutinho2012@gmail.com>
2021-12-18 12:30:57 +0100

    first commit

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { writable } from "svelte/store"
+
+export const toDoItems = writable([
+  {text: 'Take out trash', status: true},
+  {text: 'Study programming', status: false},
+  {text: 'Text Lucy', status: false}
+])
+
